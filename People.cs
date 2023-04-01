d24ca3fee42155cc354d15084e613de3641be20f
branrile09 <93716194+branrile09@users.noreply.github.com>
2023-04-01 16:46:09 +1030

    First push

---
+++ People.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    internal class People
+    {
+        public int x = 0;
+        public int y = 0;
+
+        public string Username = " ";
+
+        public People(int x, int y, string Username)
+        {
+            this.x = x;
+            this.y = y;
+            this.Username = Username;
+        }
+
+        public People(byte[] body)
+        {
+            string message = Encoding.UTF8.GetString(body);
+            string[] words = message.Split(' ');
+            Username = words[0];
+            x = int.Parse(words[1]);
+            y = int.Parse(words[2]);
+        }
+
+
+        public static byte[] SendLocation(int x, int y, string Username)
+        {
+            string message = $"{Username} {x} {y}";
+            byte[] encoded_message = Encoding.UTF8.GetBytes(message);
+            return encoded_message;
+        }
+
+        public static byte[] PersonMove(string Move, string Username)
+        {
+            string message = Username + " " + Move;
+            byte[] encoded_message = Encoding.UTF8.GetBytes(message);
+            return encoded_message;
+        }
+
+
+        public void PersonMove(string Move)
+        {
+            switch (Move)
+            {
+                case "UP":
+                    y++;
+                    break;
+
+                case "DOWN":
+                    y--;
+                    break;
+                case "LEFT":
+                    x--;
+                    break;
+
+                case "RIGHT":
+                    x++;
+                    break;
+            }
+
+        }
+
+
+
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using RabbitMQ.Client;
+using System.Numerics;
+
+namespace Person
+{
+    //randomise position
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int ActionSpeed = 1000; //1000 is equal to a second
+            
+            //Asking For User ID
+            Console.WriteLine("Whats Your Name?: ");
+            string User_Id = Console.ReadLine();
+
+            //Starting Position Creation
+            Vector2 startPos = CreateStartPosition();
+            byte[] byte_startLocation = People.SendLocation((int)startPos.X, (int)startPos.Y, User_Id);
+
+            //RabbitMq Things
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            var connection = factory.CreateConnection();
+            var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "connect",
+                     durable: true,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+
+            channel.BasicPublish(exchange: "Positions",
+                     routingKey: "connect",
+                     basicProperties: null,
+                     body: byte_startLocation);
+
+            //moving - movement
+            //connecting (starting pos) - connect 
+
+            //initial delay
+            Thread.Sleep(500);
+
+            bool Continue = true;            
+            while (Continue) {
+                
+                var dir = People.PersonMove(MoveDirection(), User_Id);
+                channel.BasicPublish(exchange: "Positions",
+                     routingKey: "movement",
+                     basicProperties: null,
+                     body: dir);
+
+                Thread.Sleep(ActionSpeed);
+            }
+
+        }
+
+
+        static string MoveDirection()
+        {           
+                Random rand = new Random();
+
+                //The four different directions
+                int RandomNumber = rand.Next(4);
+
+                string Direction = "";
+
+                switch (RandomNumber)
+                {
+                    case 0:
+                        Direction = "UP";
+                        break;
+
+                    case 1:
+                        Direction = "DOWN";
+                        break;
+
+                    case 2:
+                        Direction = "LEFT";
+                        break;
+
+                    case 3:
+                        Direction = "RIGHT";
+                        break;
+                }
+
+                return Direction;
+                
+            
+            //Send Message to Position Channel (RBITMQ CHannel)
+        }
+
+        static Vector2 CreateStartPosition()
+        {
+            Random rand = new Random(); 
+            //Maximum area is 10 but can be changed to larger number
+            int max_direction = 9;
+
+            float x = rand.Next(max_direction);
+            float y = rand.Next(max_direction);
+
+            return new Vector2(x,y);
+        }
+    }
+}
